27185fa8c09c45ba72ca6f4d56cac6fc97acdcf0
QobilovSardor <note47297@gmail.com>
2024-02-05 19:58:51 +0500

    project deskop sections  finished

--- src/components/strategy/strategy-section.tsx
+++ src/components/strategy/strategy-section.tsx
@@ -7,7 +7,7 @@ export const StrategySection = () => {
 
 			<div className="flex items-end justify-between">
 				<div className="max-w-[555px]">
-					<h1 className="section-number">02</h1>
+					<h1 className="section-number">03</h1>
 					<p className="section-number__info">Strategy</p>
 					<h2 className="title-text">Why Openchaty ?</h2>
 				</div>
@@ -19,10 +19,13 @@ export const StrategySection = () => {
 			</div>
 
 
-			<div className="video-box flex mt-[126px] items-center justify-center flex-col rounded-[43px] h-[792px] bg-[#EFEFEF] mb-[81px]">
-				<button>
-					<img className="w-[220px] h-[220px] hover:scale-105 transition-all duration-200" src={assets.playIcon} alt="play icon" />
-				</button>
+			<div className="video-box overflow-hidden flex mt-[126px] items-center justify-center flex-col rounded-[43px] h-[792px] bg-[#EFEFEF] mb-[81px]">
+				<iframe width="100%" height="100%" src="https://www.youtube.com/embed/KJwYBJMSbPI?si=5G-eHtVVngboK5gt" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+
+				</iframe>
+				{/* <button>
+						<img className="w-[220px] h-[220px] hover:scale-105 transition-all duration-200" src={assets.playIcon} alt="play icon" />
+					</button> */}
 			</div>
 		</div>
 	)
--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -1,7 +1,34 @@
+import './layout.css';
+
 export const Footer = () => {
 	return (
-		<footer>
-			
-		</footer>
+		<div className="container">
+			<footer className="pt-[122px] pb-[31px] text-center">
+				<h3>Connect your finances with</h3>
+				<h2 className='text-white text-[98px] leading-[119px] font-bold mb-[60px]'>
+					OpenChaty
+				</h2>
+				<a className="btn bg-white text-black text-[32px] mb-[88px]" href="#!">
+					Join OpenChaty now
+				</a>
+
+				<ul className='flex items-center justify-between mx-[204px]'>
+					<li>
+						<a className='py-[10px] px-[20px] text-[20px] leading-160 font-normal text-[#7C7C7C] bg-[#111]' href="#!">
+							Terms & Agreements
+						</a>
+					</li>
+					<li className='py-[10px] px-[20px] text-[20px] leading-160 font-normal text-[#7C7C7C]'>
+						©2024 OpenChaty All Rights Reserved
+					</li>
+					<li>
+						<a className='py-[10px] px-[20px] text-[20px] leading-160 font-normal text-[#7C7C7C] bg-[#111]' href="#!">
+							Privacy Policy
+						</a>
+					</li>
+				</ul>
+
+			</footer>
+		</div>
 	)
 }
